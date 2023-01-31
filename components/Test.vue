7bf58455598f7cb615ec44168d98337f4e09d575
liuhuaifu <huafull0578@163.com>
2023-01-31 16:19:36 +0800

    feat: 添加测试组件

---
+++ components/Test.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const text = 12344;
+</script>
+
+<template>
+  <div>
+    {{ text }}
+    <div>测试组件</div>
+  </div>
+</template>
---
+++ theme/index.ts
@@ -0,0 +1,8 @@
+import type { Theme } from "vitepress";
+import Test from "@/components/Test.vue";
+
+export default {
+  enhanceApp(ctx) {
+    ctx.app.component("Test", Test);
+  },
+} as Theme;
