c56e8826ed1dea99a929c5a273407a76784b4c8a
liuhuaifu <huafull0578@163.com>
2023-02-10 17:07:58 +0800

    feat: 添加全局注入

--- config/head/index.ts
+++ config/head/index.ts
@@ -1,1 +1,39 @@
-export default [];
+import { readdirSync, readFileSync } from "fs";
+import { resolve } from "path";
+
+import { isProdEnv } from "../../utils/env";
+
+import type { HeadConfig } from "vitepress";
+
+function getFiles() {
+  const dirsMap = {
+    css: {
+      path: resolve(__dirname, "css"),
+      resolver: (content: string, file: string) => {
+        return [
+          "style",
+          { type: "text/css", ...(!isProdEnv && { "data-vite-dev-id": file }) },
+          content,
+        ];
+      },
+    },
+    js: {
+      path: resolve(__dirname, "js"),
+      resolver: (content: string, file: string) => {
+        return ["script", { type: "module" }, content];
+      },
+    },
+  };
+
+  return Object.entries(dirsMap).reduce((arr, [key, config]) => {
+    const files = readdirSync(config.path, { encoding: "utf-8" });
+    const result = files?.map((filename) => {
+      const file = resolve(config.path, filename);
+      const content = readFileSync(file, { encoding: "utf-8" });
+      return config.resolver(content, file);
+    });
+    return arr.concat(result);
+  }, [] as any[]);
+}
+
+export default [...getFiles()] as HeadConfig[];
