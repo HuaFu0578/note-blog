4005173149706f25ceb154418247dcb902a991ee
liuhuaifu <huafull0578@163.com>
2023-02-12 11:33:11 +0800

    feat: 完善 node-ts 模板

--- config/markdown/plugins/codesandbox/customTemplates.ts
+++ config/markdown/plugins/codesandbox/customTemplates.ts
@@ -7,7 +7,7 @@ interface CustomTemplateInfo extends Omit<TemplateInfo, "extends"> {
 export default {
   "react-component": { extends: "new", entry: "src/App.js" },
   js: { extends: "file:./js" },
-  "node-ts": { extends: "file:./node-ts", entry: "src/index.ts" },
+  "node-ts": { extends: "file:./node-ts", entry: "main.ts" },
   ts: { extends: "file:./ts" },
   react: { extends: "new" },
   vue: { extends: "file:./vue", entry: "src/App.vue" },
--- config/markdown/plugins/codesandbox/templates/node-ts/src/index.ts
+++
@@ -1,2 +0,0 @@
-const ad = { a: 2, d: 3, rrr: 3 };
-console.log("test node-ts aaa haldadlf", ad, "daldkfjdsl");
